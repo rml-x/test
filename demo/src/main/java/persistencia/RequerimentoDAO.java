50646c7af0cb10bcf538540823ee8f85efa0eb19
rml-x <cristiane.xavier@aluno.riogrande.ifrs.edu.br>
2026-04-27 03:43:25 -0300

    reqlists

--- demo/src/main/java/persistencia/RequerimentoDAO.java
+++ demo/src/main/java/persistencia/RequerimentoDAO.java
@@ -33,7 +33,7 @@ public class RequerimentoDAO implements IRequerimento {
                 req.setObservacao(rs.getString("observacao"));
                 req.setDataHoraAbertura(rs.getTimestamp("data_hora_abertura"));
                 req.setStatus(rs.getString("status"));
-
+                
                
                 Aluno aluno = new Aluno();
                 aluno.setMatricula(rs.getString("matricula_aluno"));
@@ -66,9 +66,51 @@ public class RequerimentoDAO implements IRequerimento {
 
     }
 
-    public List<Requerimento>listarRequerimentoPorAluno(String matricula) throws SQLException{
+    public List<Requerimento> listarRequerimentoPorAluno(String matricula) throws SQLException {
+        
+        String sql = "SELECT r.*, u.nome AS nome_aluno, c.nome AS nome_curso, tr.descricao AS tipo_descricao FROM requerimento r INNER JOIN aluno a ON r.matricula_aluno = a.matricula INNER JOIN usuario u ON a.usuario_id = u.id INNER JOIN curso c ON a.curso_id = c.id INNER JOIN tipo_requerimento tr ON r.tipo_id = tr.id WHERE r.ativo IS TRUE AND r.matricula_aluno = ? ORDER BY r.id DESC";
 
+        List<Requerimento> reqPerAluno = new ArrayList<>();
+
+        try (Connection conn = new ConexaoPostgreSQL().getConexao();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+            
+            stmt.setString(1, matricula);
+
+            try (ResultSet rs = stmt.executeQuery()) { 
+                while (rs.next()) {
+                    Requerimento req = new Requerimento();
+                    req.setId(rs.getInt("id"));
+                    req.setObservacao(rs.getString("observacao"));
+                    req.setDataHoraAbertura(rs.getTimestamp("data_hora_abertura"));
+                    req.setStatus(rs.getString("status"));
+
+                    // Aluno
+                    Aluno aluno = new Aluno();
+                    aluno.setMatricula(rs.getString("matricula_aluno"));
+                    
+                    Usuario usu = new Usuario();
+                    usu.setNome(rs.getString("nome_aluno"));
+                    aluno.setUsuario(usu);
+
+                    Curso curso = new Curso();
+                    curso.setNome(rs.getString("nome_curso"));
+                    aluno.setCurso(curso);
+                    req.setAluno(aluno);
+
+                    // Tipo
+                    TipoRequerimento tipo = new TipoRequerimento();
+                    tipo.setId(rs.getInt("tipo_id"));
+                    tipo.setDescricao(rs.getString("tipo_descricao"));
+                    req.setTipo(tipo);
+
+                    reqPerAluno.add(req);
+                }
+            }
+        }
+        return reqPerAluno;
     }
+    
 
     public void atualizar(Requerimento requerimento) throws SQLException{
 
